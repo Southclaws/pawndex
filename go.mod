eee426ab0708539f5b3bcf39b32a64eb1ffcd52b
Southclaws <hello@southcla.ws>
2020-05-12 20:39:29 +0100

    rewrite service to use new components

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module github.com/Southclaws/pawndex
 
+go 1.14
+
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/Masterminds/semver v1.4.2
@@ -7,31 +9,25 @@ require (
 	github.com/Southclaws/sampctl v0.0.0-20190127125824-a496adef9845
 	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/gliderlabs/ssh v0.1.3 // indirect
+	github.com/go-chi/chi v4.1.1+incompatible
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/google/uuid v1.1.0 // indirect
 	github.com/gorilla/handlers v1.4.0
-	github.com/gorilla/mux v1.7.0
 	github.com/joho/godotenv v1.3.0
 	github.com/kelseyhightower/envconfig v1.3.0
 	github.com/mattn/go-colorable v0.1.1 // indirect
 	github.com/pkg/errors v0.8.1
-	github.com/prometheus/client_golang v0.9.2
-	github.com/prometheus/client_model v0.0.0-20190129233127-fd36f4220a90 // indirect
-	github.com/prometheus/common v0.2.0 // indirect
-	github.com/prometheus/procfs v0.0.0-20190219184716-e4d4a2206da0 // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/xanzy/ssh-agent v0.2.1 // indirect
+	go.etcd.io/bbolt v1.3.4
 	go.uber.org/atomic v1.3.2 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.9.1
 	golang.org/x/crypto v0.0.0-20190222235706-ffb98f73852f // indirect
 	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd // indirect
 	golang.org/x/oauth2 v0.0.0-20190220154721-9b3c75971fc9
-	golang.org/x/sys v0.0.0-20190222171317-cd391775e71e // indirect
 	golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2 // indirect
 	gopkg.in/src-d/go-git-fixtures.v3 v3.3.0 // indirect
 	gopkg.in/src-d/go-git.v4 v4.10.0 // indirect
-	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0
-	gopkg.in/yaml.v2 v2.2.2 // indirect
+	gopkg.in/yaml.v2 v2.2.2
 )
--- service/service.go
+++ service/service.go
@@ -2,63 +2,75 @@ package service
 
 import (
 	"context"
-	"strings"
-	"sync"
 	"time"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
+
+	"github.com/Southclaws/pawndex/api"
+	"github.com/Southclaws/pawndex/daemon"
+	"github.com/Southclaws/pawndex/scraper"
+	"github.com/Southclaws/pawndex/searcher"
+	"github.com/Southclaws/pawndex/storage"
 )
 
 // App stores the app state
 type App struct {
-	config   Config
-	gh       *github.Client
-	toScrape chan github.Repository
-	toIndex  chan Package
-	index    map[string]Package
-	lock     sync.RWMutex
-	metrics  Metrics
+	config Config
+	gh     *github.Client
+	server api.Server
+	daemon daemon.Daemon
 }
 
 // Config stores static configuration
 type Config struct {
 	Bind           string        `required:"true"` // bind interface
-	Domain         string        `required:"true"` // public domain
 	GithubToken    string        `required:"true"` // GitHub API token
 	SearchInterval time.Duration `required:"true"` // interval between checks
 	ScrapeInterval time.Duration `required:"true"` // interval between scrapes
-	Cache          string        `required:"true"` // cache for persistence
+	DatabasePath   string        `required:"true"` // cache for persistence
 }
 
 // Initialise prepres the service for starting
 func Initialise(ctx context.Context, config Config) (app *App, err error) {
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: config.GithubToken})
-	tc := oauth2.NewClient(ctx, ts)
-
-	app = &App{
-		config:   config,
-		gh:       github.NewClient(tc),
-		toScrape: make(chan github.Repository, 2000),
-		toIndex:  make(chan Package, 2000),
-		index:    make(map[string]Package),
-		lock:     sync.RWMutex{},
-		metrics:  newMetrics(),
+	gh := github.NewClient(oauth2.NewClient(ctx, oauth2.StaticTokenSource(&oauth2.Token{AccessToken: config.GithubToken})))
+	search := searcher.GitHubSearcher{GitHub: gh}
+	scrape := scraper.GitHubScraper{GitHub: gh}
+	store, err := storage.New(config.DatabasePath)
+	if err != nil {
+		return nil, err
 	}
 
-	err = app.loadCache()
-	if err != nil && !strings.HasSuffix(err.Error(), "no such file or directory") {
-		return
-	}
-	app.metrics.IndexSize.Set(float64(len(app.index)))
-
-	return
+	return &App{
+		config: config,
+		gh:     gh,
+		server: api.New(config.Bind, store),
+		daemon: daemon.Daemon{
+			Searcher:       &search,
+			Scraper:        &scrape,
+			Storer:         store,
+			SearchInterval: config.SearchInterval,
+			ScrapeInterval: config.ScrapeInterval,
+		},
+	}, nil
 }
 
 // Start initialises the app and blocks until fatal error
-func (app *App) Start() (err error) {
-	go app.runServer()
-	app.updateList([]string{"topic:pawn-package", "language:pawn", "topic:sa-mp"})
-	app.Daemon()
-	return
+func (app *App) Start(ctx context.Context) (err error) {
+	errs := make(chan error)
+
+	go func() {
+		errs <- app.server.Run()
+	}()
+
+	go func() {
+		app.daemon.Run(ctx)
+	}()
+
+	select {
+	case err := <-errs:
+		return err
+	case <-ctx.Done():
+		return context.Canceled
+	}
 }
